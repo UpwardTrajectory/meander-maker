aa1af62f5d5381a271695a5c6f3ed4b9b1c72965
“David <dakaspar@gmail.com>
2019-07-09 20:29:17 -0700

    reroute html to saved file

--- meander_maker/webapp/app.py
+++ meander_maker/webapp/app.py
@@ -1,4 +1,5 @@
-import base64
+#import base64
+import uuid
 from flask import Flask, request, render_template, jsonify
 from .. import goplaces as gp
 
@@ -18,9 +19,14 @@ def generate_output():
     loc, topic, mode = data['loc'], data['topic'], data['mode']
     html_map = gp.all_things(
         loc, topic, mode, n=20, verbose=False, output='flask')
-    html_map_b64 = base64.b64encode(html_map.encode('utf-8'))
-    return (
-        b'''<iframe src="data:text/html;charset=utf-8;base64,'''
-        + html_map_b64
-        + b'''"></iframe>'''
-    )
+#     html_map_b64 = base64.b64encode(html_map.encode('utf-8'))
+#     return (
+#         b'''<iframe src="data:text/html;charset=utf-8;base64,'''
+#         + html_map_b64
+#         + b'''"></iframe>'''
+#     )
+    map_id = uuid.uuid4()
+    with open(f'meander_maker/webapp/static/maps/{map_id}.html', 'w') as f:
+        f.write(html_map)
+    return f'<iframe src="/maps/{map_id}.html"></iframe>'
+    
